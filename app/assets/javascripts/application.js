2225013f5b98a381781711cf4808b2289646cd6a
Ryan Strandt <ryan.strandt@cru.org>
2016-02-12 13:25:57 -0500

    remove sprokets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,5 @@
-//= require /node_modules/angular2/bundles/angular2-polyfills.js
-//= require /node_modules/systemjs/dist/system.src.js
-//= require /node_modules/rxjs/bundles/Rx.js
-//= require /node_modules/angular2/bundles/angular2.dev.js
-//= require /node_modules/angular2/bundles/router.dev.js
+//= require angular2/bundles/angular2-polyfills.js
+//= require systemjs/dist/system.src.js
+//= require rxjs/bundles/Rx.js
+//= require angular2/bundles/angular2.dev.js
+//= require angular2/bundles/router.dev.js
--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Alice
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.enabled = false
+    config.assets.paths << Rails.root.join("node_modules")
 
   end
 end
