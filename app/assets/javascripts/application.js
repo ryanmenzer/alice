3179015f2e12e33e2bf70cda26f20fff5b61d039
Ryan Strandt <ryan.strandt@cru.org>
2016-02-17 16:26:25 -0500

    add es6-shim

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,5 @@
+//= require es6-shim/es6-shim.min.js
+//= require systemjs/dist/system-polyfills.js
 //= require angular2/bundles/angular2-polyfills.js
 //= require systemjs/dist/system.src.js
 //= require rxjs/bundles/Rx.js
